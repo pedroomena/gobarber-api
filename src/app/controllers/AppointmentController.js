0204d55cf46f2a4a8f340750ff29e449cfa62f3a
Pedro Vieira <pedro@os3ti.com>
2019-11-24 12:33:32 -0300

    Add mail sender
    - nodemailer
    - mailhog running in a docker container

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
@@ -122,7 +124,15 @@ class AppointmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email']
+                }
+            ]
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({
@@ -141,6 +151,12 @@ class AppointmentController {
         appointment.canceled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'An appointment has been canceled',
+            text: `The user ${appointment.user_id} canceled an appointment.`
+        });
 
         return res.json(appointment);
     }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'localhost',
+    port: '1025',
+    secure: false,
+    auth: {
+        user: '',
+        password: ''
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>'
+    }
+};
